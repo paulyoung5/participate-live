0822fa6a67b2aae34a32f8884183828ee5fbe1ac
PAUL <paul-young@live.co.uk>
2017-01-11 13:17:31 +0000

    Added postgresql

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
 app.use(express.static(__dirname + '/public'));
 
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
